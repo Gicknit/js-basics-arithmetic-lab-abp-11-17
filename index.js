40dfc0c12565c12b0a27b1111f2552efe3986f20
Nick Garcia <89nickgarcia@gmail.com>
2017-11-10 05:03:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ const randomNumber = Math.random();
 
 const randomInteger = Math.round(Math.floor(randomNumber));
 
-const randomUserID = Math.isInteger(1000000000 + randomInteger);
+const randomUserID = Math.isInteger(1000000000 + randomInteger)*20;
