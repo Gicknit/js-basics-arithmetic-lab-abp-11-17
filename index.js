a639c5ea0a99faaeecbfbd0006dcfcf4b62a864a
Nick Garcia <89nickgarcia@gmail.com>
2017-11-10 04:32:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ const randomNumber = Math.random();
 
 const randomInteger = Math.round(Math.floor(randomNumber));
 
-const randomUserID = Number.isNaN(randomInteger);
+const randomUserID = Number.isNaN(randomInteger)*20;
